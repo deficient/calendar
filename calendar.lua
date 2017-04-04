dda7162e85345d93d6faad54738a1d7424b35da4
Thomas Gläßle <t_glaessle@gmx.de>
2017-04-04 17:48:32 +0200

    Remove global state and make it a proper component

--- calendar.lua
+++ calendar.lua
@@ -4,143 +4,133 @@
 --   + transformed to module
 --   + the current day formating is customizable
 
-local string = string
---local print = print
-local tostring = tostring
+-- captures
 local os = os
 local capi = {
     mouse = mouse,
-    screen = screen
+    screen = screen,
 }
 local awful = require("awful")
 local naughty = require("naughty")
 
-local calendar = {}
-local dformat = {
-    today = '<b><span color="#00ff00">%2i</span></b>',
-    anyday = '%2i'
-}
 
-local function fdate(format, table)
-    return os.date(format, os.time(table))
+-- utility functions
+local function format_date(format, date)
+    return os.date(format, os.time(date))
 end
 
-local function displayMonth(month, year, weekStart)
-    local highlightRequire = "%m-%d"
-    local today = os.date(highlightRequire)
-    -- weekStart=1 <=> monday; 2001 started with a monday
-    local tA = os.time({year=year, month=month, day=1})
-    local d0 = fdate("*t", {year=2001, month=1, day=weekStart})
-    local dA = fdate("*t", {year=year, month=month, day=1})
-    local dB = fdate("*t", {year=year, month=month+1, day=0})
+local function day_id(date)
+    return format_date("%m-%d", date)
+end
+
+local calendar = {}
+
+function calendar:new(args)
+    return setmetatable({}, {__index = self}):init(args)
+end
+
+function calendar:init(args)
+    -- first day of week: monday=1, …, sunday=7
+    self.fdow       = args.fdow       or 1
+    self.html       = args.html       or '<span font_desc="monospace">%s</span>'
+    self.today      = args.today      or '<b><span color="#00ff00">%2i</span></b>'
+    self.anyday     = args.anyday     or '%2i'
+    self.page_title = args.page_title or '%B %Y\n\n'
+    self.col_title  = args.col_title  or '%a '
+    return self
+end
+
+function calendar:page(month, year)
+
+    local today = day_id()
+
+    -- 2001 started with a monday:
+    local d0 = format_date("*t", {year=2001, month=1,       day=self.fdow })
+    local dA = format_date("*t", {year=year, month=month,   day=1         })
+    local dB = format_date("*t", {year=year, month=month+1, day=0         })
+    local tA =                   {year=year, month=month,   day=1         }
     local colA = (dA.wday - d0.wday) % 7
 
-    -- print header
-    local page = os.date("%B %Y\n", tA)
+    -- print page title
+    local page = format_date(self.page_title, tA)
 
-    -- print week-day names (table head row)
-    page = page .. "\n    "
-    for x = 0,6 do
-        page = page .. fdate("%a ", {year=d0.year, month=d0.month, day=d0.day+x})
+    -- print column titles (weekday)
+    page = page .. "    "
+    for d = 0, 6 do
+        page = page .. format_date(self.col_title, {
+            year  = d0.year,
+            month = d0.month,
+            day   = d0.day + d,
+        })
     end
 
     -- print empty space before first day
-    page = page .. "\n" .. os.date(" %V", tA)
-    local column = 0
-    while column < colA do
+    page = page .. "\n" .. format_date(" %V", tA)
+    for column = 1, colA do
         page = page .. "   -"
-        column = column + 1
     end
 
     -- iterate all days of the month
     local nLines = 1
-    for day = 1,dB.day do
+    local column = colA
+    for day = 1, dB.day do
         if column == 7 then
             column = 0
             nLines = nLines + 1
-            page = page .. "\n" .. fdate(" %V", {year=year, month=month, day=day})
+            page = page .. "\n" .. format_date(" %V", {year=year, month=month, day=day})
         end
-        if today == fdate(highlightRequire, {day=day, month=month, year=year}) then
-            page = page .. "  " .. dformat.today:format(day)
+        if today == day_id {day=day, month=month, year=year} then
+            page = page .. "  " .. self.today:format(day)
         else
-            page = page .. "  " .. dformat.anyday:format(day)
+            page = page .. "  " .. self.anyday:format(day)
         end
         column = column + 1
     end
-    while column < 7 do
+
+    for column = column, 6 do
         page = page .. "   -"
-        column = column + 1
     end
+
     return page
 end
 
-local function switchNaughtyMonth(switchMonths)
-    if (#calendar < 3) then return end
-    local swMonths = switchMonths or 1
-
-    local month = calendar[1] + swMonths
-    local year = calendar[2]
-
-    calendar[1] = month
-    -- TODO: just redraw calendar instead of recreating entirely
-    naughty.destroy(calendar[3])
-    calendar[3] = naughty.notify({
-                text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(month, year, 1)),
-                timeout = 0,
-                hover_timeout = 0.5,
-                screen = capi.mouse.screen
-            })
-
-    -- the following does NOT work (doesnt recalculate boundaries):
-    -- calendar[3].box.widgets[2].text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(calendar[1], calendar[2], 1))
+function calendar:switch(months)
+    self:show(self.year, self.month+months)
 end
 
-local function addCalendarToWidget(mywidget, custom_current_day_format)
-    if custom_current_day_format then
-        dformat.today = custom_current_day_format
-    end
+function calendar:show(year, month)
+    local today = os.time()
+    self.month  = month or os.date('%m', today)
+    self.year   = year  or os.date('%Y', today)
 
-    mywidget:connect_signal('mouse::enter', function ()
-        local month, year = os.date('%m'), os.date('%Y')
-        calendar = { month, year,
-            naughty.notify({
-                text = string.format('<span font_desc="%s">%s</span>', "monospace", displayMonth(month, year, 1)),
-                timeout = 0,
-                hover_timeout = 0.5,
-                screen = capi.mouse.screen
-            })
-        }
-    end)
-    mywidget:connect_signal('mouse::leave', function () naughty.destroy(calendar[3]) end)
+    self:hide()
+    self.notification = naughty.notify({
+        text = self.html:format(self:page(self.month, self.year)),
+        timeout = 0,
+        hover_timeout = 0.5,
+        screen = capi.mouse.screen
+    })
+end
 
-    mywidget:buttons(awful.util.table.join(
-        awful.button({ }, 1, function()
-            switchNaughtyMonth(-1)
-        end),
-        awful.button({ }, 3, function()
-            switchNaughtyMonth(1)
-        end),
-        awful.button({ }, 4, function()
-            switchNaughtyMonth(-1)
-        end),
-        awful.button({ }, 5, function()
-            switchNaughtyMonth(1)
-        end),
-        awful.button({ 'Shift' }, 1, function()
-            switchNaughtyMonth(-12)
-        end),
-        awful.button({ 'Shift' }, 3, function()
-            switchNaughtyMonth(12)
-        end),
-        awful.button({ 'Shift' }, 4, function()
-            switchNaughtyMonth(-12)
-        end),
-        awful.button({ 'Shift' }, 5, function()
-            switchNaughtyMonth(12)
-        end)
+function calendar:hide()
+    naughty.destroy(self.notification)
+end
+
+function calendar:attach(widget)
+    widget:connect_signal('mouse::enter', function() self:show() end)
+    widget:connect_signal('mouse::leave', function() self:hide() end)
+    widget:buttons(awful.util.table.join(
+        awful.button({         }, 1, function() self:switch( -1) end),
+        awful.button({         }, 3, function() self:switch(  1) end),
+        awful.button({         }, 4, function() self:switch( -1) end),
+        awful.button({         }, 5, function() self:switch(  1) end),
+        awful.button({ 'Shift' }, 1, function() self:switch(-12) end),
+        awful.button({ 'Shift' }, 3, function() self:switch( 12) end),
+        awful.button({ 'Shift' }, 4, function() self:switch(-12) end),
+        awful.button({ 'Shift' }, 5, function() self:switch( 12) end)
     ))
 end
 
-return {
-  addCalendarToWidget=addCalendarToWidget,
-}
+return setmetatable(calendar, {
+    __call = calendar.new,
+})
